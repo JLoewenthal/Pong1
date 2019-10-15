94b8771aaed4cb6380ad20c81a08d1434c8ca831
JL <joshloewenthal@gmail.com>
2019-10-15 22:16:58 +0200

    Video #2 Main Window Graphics

--- Pong1/Source.cpp
+++ Pong1/Source.cpp
@@ -4,6 +4,10 @@
 
 bool running = true;
 
+void* bufferMemory;
+int bufferWidth;
+int bufferHeight;
+BITMAPINFO bufferBitmapInfo;
 
 
 LRESULT CALLBACK window_callback(
@@ -20,6 +24,28 @@ LRESULT CALLBACK window_callback(
 	case WM_DESTROY: {
 		running = false;
 	} break;
+
+	case WM_SIZE: {
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		bufferWidth = rect.right - rect.left;
+		bufferHeight = rect.bottom - rect.top;
+
+		int bufferSize = bufferWidth * bufferHeight * sizeof(unsigned int);
+
+		if (bufferMemory) {
+			VirtualFree(bufferMemory, 0, MEM_RELEASE);
+		}
+		bufferMemory = VirtualAlloc(0, bufferSize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+		bufferBitmapInfo.bmiHeader.biSize = sizeof(bufferBitmapInfo.bmiHeader);
+		bufferBitmapInfo.bmiHeader.biWidth = bufferWidth;
+		bufferBitmapInfo.bmiHeader.biHeight = bufferHeight;
+		bufferBitmapInfo.bmiHeader.biPlanes = 1;
+		bufferBitmapInfo.bmiHeader.biBitCount = 32;
+		bufferBitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	} break;
 	
 	default: {
 		result = DefWindowProc(hwnd, uMsg, wParam, lParam);
@@ -30,7 +56,7 @@ LRESULT CALLBACK window_callback(
 };
 
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+int _stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
 
 	// Create Window Class
 	WNDCLASS windows_class = {};
@@ -40,7 +66,7 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	// Register Class
 	RegisterClass(&windows_class);
 	// Create Window
-	HWND gameWindow = CreateWindowA(
+	HWND gameWindow = CreateWindow(
 		windows_class.lpszClassName,
 		"Pong Game Window",
 		WS_OVERLAPPEDWINDOW | WS_VISIBLE,
@@ -54,6 +80,8 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		0
 	);
 
+	HDC hdc = GetDC(gameWindow);
+
 	while (running) {
 
 		// Inpute
@@ -63,7 +91,15 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 			DispatchMessage(&message);
 		}
 		//Simulate
+		unsigned int* pixel = (unsigned int*)bufferMemory;
+		for (int y = 0; y < bufferHeight; y++) {
+			for (int x = 0; x < bufferWidth; x++) {
+				*pixel++ = x * y;
+			}
+
+		}
 
 		//Render
+		StretchDIBits(hdc, 0, 0, bufferWidth, bufferHeight, 0, 0, bufferWidth, bufferHeight, bufferMemory, &bufferBitmapInfo, DIB_RGB_COLORS, SRCCOPY);
 	}
 };
